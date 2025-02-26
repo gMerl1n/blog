26f8bf5bd545a9070caba6e23e98ed7e2edac8ee
Vladislav <talcore@yandex.ru>
2025-02-26 21:56:49 +0500

    add: token manager added

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,82 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+// TokenManager provides logic for JWT & Refresh tokens generation and parsing.
+type ITokenManager interface {
+	NewJWT(userID string) (string, error)
+	Parse(accessToken string) (string, error)
+	NewRefreshToken() (string, error)
+}
+
+type TokenManager struct {
+	signingKey      string
+	accessTokenTTL  time.Duration
+	refreshTokenTTL time.Duration
+}
+
+func NewManager(signingKey string, accessTokenTTL time.Duration, refreshTokenTTL time.Duration) (*TokenManager, error) {
+	if signingKey == "" {
+		return nil, errors.New("empty signing key")
+	}
+
+	return &TokenManager{
+		signingKey:      signingKey,
+		accessTokenTTL:  accessTokenTTL,
+		refreshTokenTTL: refreshTokenTTL}, nil
+}
+
+func (m *TokenManager) NewJWT(userID string) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   userID,
+		ExpiresAt: time.Now().Add(m.accessTokenTTL * time.Minute).Unix(),
+	})
+
+	return token.SignedString([]byte(m.signingKey))
+}
+
+func (m *TokenManager) Parse(accessToken string) (string, error) {
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(m.signingKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error get user claims from token")
+	}
+
+	return claims["sub"].(string), nil
+}
+
+func (m *TokenManager) NewRefreshToken() (string, error) {
+	b := make([]byte, 32)
+
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+
+	if _, err := r.Read(b); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", b), nil
+}
