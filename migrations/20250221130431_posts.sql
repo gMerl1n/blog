f25f015318937c8bdb840f258a96a218c05439bd
Vladislav <talcore@yandex.ru>
2025-02-26 20:59:34 +0500

    add: reference key user id in posts table

--- migrations/20250221130431_posts.sql
+++ migrations/20250221130431_posts.sql
@@ -2,6 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE posts (
     id SERIAL UNIQUE NOT NULL,
+    user_id  INTEGER REFERENCES users(id) ON DELETE CASCADE;
     title VARCHAR(255) UNIQUE NOT NULL,
     body VARCHAR(255) UNIQUE NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
